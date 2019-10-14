cb8515c92b38602cc2b44a2cc508c3269bc84704
brogrammer2 <53489820+brogrammer2@users.noreply.github.com>
2019-10-14 19:01:58 +0200

    Add files via upload
    Enkel kalkulator

---
+++ kalkulator.js
@@ -0,0 +1,24 @@
+var nr1 = 10;
+var nr2 = 5;
+
+function add(a,b) {
+	return a + b;
+	}
+	
+function sub(a,b) {
+	return a - b;
+}
+
+function multiply(a,b) {
+	return a * b;
+}
+
+function split(a,b) {
+	return a / b;
+}
+
+add(nr1,nr2);
+sub(nr1,nr2);
+multiply(nr1,nr2);
+split(nr1,nr2);
+
