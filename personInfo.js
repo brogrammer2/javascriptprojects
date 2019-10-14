5934ec7cb1470a0315f10d57e2b9446a57a1983b
brogrammer2 <53489820+brogrammer2@users.noreply.github.com>
2019-10-14 18:54:48 +0200

    Add files via upload
    Forteller deg ditt navn, fodselsdag, alder og kjonn.

---
+++ personInfo.js
@@ -0,0 +1,9 @@
+var navn = prompt("Hva heter du?");
+var alder = prompt("hvor gammel er du?");
+var kjonn = prompt("Hvilket kjonn er du?");
+var fodselsdag = prompt("Når ble du født?");
+alert(
+"Ditt navn er " + navn + ". Du ble født " + fodselsdag +
+", så du er " + alder + " år gammel. Kjønnet ditt er " +
+kjonn + "."  
+);
